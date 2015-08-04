ad8ed75a9fef1b3dfdb99d1a0f2d2cfb72e086e6
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2015-08-03 21:36:25 -0400

    Support for remote install

--- setup.sh
+++ setup.sh
@@ -2,11 +2,20 @@
 #
 # Sets up the machine with essential utilities and bootstraps the dotfiles.
 
-export ZSH=$HOME/.dotfiles
-
 # Install gcc, git and zsh if this is Linux
 if [[ $(uname) == 'Linux' ]]
 then sudo apt-get install build-essential git zsh; fi
+
+# Clone/pull the jluchiji/dotfiles
+if [[ -d $HOME/.dotfiles ]]
+then
+  ( cd $HOME/.dotfiles; git pull; )
+else
+  git clone https://github.com/jluchiji/dotfiles ~/.dotfiles
+fi
+
+# Setup the envar for dotfiles dir
+export ZSH=$HOME/.dotfiles
 
 # Setup the dotfiles
 $ZSH/script/bootstrap
