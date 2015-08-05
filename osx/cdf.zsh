357c5f4eba7aaef9764a5d260a38f42c65711d58
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2015-08-04 23:48:58 -0400

    Added cdf, cd to the top Finder window

---
+++ osx/cdf.zsh
@@ -0,0 +1,22 @@
+#
+# cd to the top finder window
+#
+
+if [[ $(uname) == 'Darwin' ]]; then
+
+cdf () {
+    currFolderPath=$( /usr/bin/osascript <<EOT
+        tell application "Finder"
+            try
+        set currFolder to (folder of the front window as alias)
+            on error
+        set currFolder to (path to desktop folder as alias)
+            end try
+            POSIX path of currFolder
+        end tell
+EOT
+    )
+    cd "$currFolderPath"
+}
+
+fi
