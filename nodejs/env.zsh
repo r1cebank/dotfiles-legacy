10ab6761e3a0a27a98bde95a3bee1cb9246cd2e6
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2015-08-06 21:18:13 -0400

    Added local npm executables to the PATH

--- nodejs/env.zsh
+++ nodejs/env.zsh
@@ -1,5 +1,8 @@
-# Setup PATH to include npm global modules
-export PATH="$HOME/.npm/bin:$PATH"
+# Setup PATH to include npm global modules and local npm executables
+export PATH="./node_modules/.bin:$HOME/.npm/bin:$PATH"
 
 # Setup MANPATH to include npm global modules
 export MANPATH="$HOME/.npm/share/man:$MANPATH"
+
+# If I use these dotfiles, most likely this is a development machine
+export NODE_ENV="development"
