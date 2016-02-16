ebd6cce92f02fe800c3787202dabb3b3de626587
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2016-02-16 17:02:54 -0500

    Fixed a few minor issues

--- nodejs/aliases.zsh
+++ nodejs/aliases.zsh
@@ -5,7 +5,7 @@
 alias npm-i='npm install --save'
 alias npm-d='npm install --save-dev'
 alias npm-g='npm install --global'
-
+alias nrd='npm run dev'
 
 # ---------------------------------------------------------------------------- #
 # Add heroku to the PATH.                                                      #
