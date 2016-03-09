72c13984f0c4aefd2ecbdf6b5426b0ce467dd1e1
Siyuan Gao <sgao@mobify.com>
2016-03-09 13:00:58 -0800

    Removing unused zsh aliases

--- functions/heroku-bunyan.zsh
+++
@@ -1,9 +0,0 @@
-#
-# Heroku-bunyan log watcher.
-#
-# Author:  Denis Luchkin-Zhou <wyvernzora@gmail.com>
-# License: MIT
-
-heronyan() {
-  heroku logs $@ | sed 's/.*app\[web\..*\]\: //' | bunyan
-}
