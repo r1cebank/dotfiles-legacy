5d3802efdaadc62be4d78b1ce27d042d48d08114
Siyuan Gao <siyuangao@gmail.com>
2016-03-09 22:30:48 -0800

    Fixed absolute path bug

--- mas/post.sh
+++ mas/post.sh
@@ -11,7 +11,7 @@ if type "apm" > /dev/null; then
   echo "  Installing App Store apps."
   while read p; do
     mas install $p
-  done < ./applications.list
+done < $ZSH/mas/applications.list
 else
   echo "  Did not find mas, skipping app store applications."
 fi
