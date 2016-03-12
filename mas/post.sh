1f40e40707a72f7e9a8d0dd4d4b40274dba12b17
Siyuan Gao <siyuangao@gmail.com>
2016-03-12 13:37:30 -0800

    Update post.sh

--- mas/post.sh
+++ mas/post.sh
@@ -5,13 +5,13 @@
 # This will install all apps from app store
 
 # Check for mas
-if type "apm" > /dev/null; then
+if type "mas" > /dev/null; then
   echo "  Updating existing App Store apps"
   mas upgrade
   echo "  Installing App Store apps."
   while read p; do
     mas install $p
-done < $ZSH/mas/applications.list
+  done < $ZSH/mas/applications.list
 else
   echo "  Did not find mas, skipping app store applications."
 fi
