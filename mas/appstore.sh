04b8fcac95a3e03896202a8cf67d18393ed9e1d9
Siyuan Gao <sgao@mobify.com>
2016-03-14 16:49:03 -0700

    mas install status, so will not reinstall existing ones

--- mas/appstore.sh
+++ mas/appstore.sh
@@ -10,7 +10,12 @@ if type "mas" > /dev/null; then
   mas upgrade
   echo "  Installing App Store apps."
   while read p; do
-    mas install $p
+    if [ ! -f $ZSH/mas/$p ]; then
+        mas install $p
+        touch $ZSH/mas/$p
+    else
+        echo "Skipping "$p
+    fi
   done < $ZSH/mas/applications.list
 else
   echo "  Did not find mas, skipping app store applications."
