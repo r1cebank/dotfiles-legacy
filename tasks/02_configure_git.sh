f5c95cbfb2e4f69803b971325ff9d76782302d07
r1cebank <siyuangao@gmail.com>
2017-12-17 02:02:40 -0800

    update git setup

--- tasks/02_configure_git.sh
+++ tasks/02_configure_git.sh
@@ -8,15 +8,13 @@ function configure_git_run() {
     PLATFORM=$(settings_get "PLATFORM")
     if ! [ -f git/gitconfig.symlink ]
     then
-        git_credential='cache'
-        if [ "$PLATFORM" = "debian" ]; then
-            git_credential='osxkeychain'
-        fi
 
         git_authorname=$(enter_variable " - What is your github author name?" "$(whoami)")
         git_authoremail=$(enter_variable " - What is your github author email?" "$(whoami)@gmail.com")
+        git_gpgsign=$(enter_variable " - Do you want to enable commit signing? (true, false)" "true")
+        git_gpgkey=$(enter_variable " - What is your signingkey?" "$(gpg --list-secret-keys --with-colons 2> /dev/null | grep '^sec:' | cut -d':' -f 5)")
 
-        sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GIT_CREDENTIAL_HELPER/$git_credential/g" $DOTFILES_ROOT/git/gitconfig.symlink.example > $DOTFILES_ROOT/git/gitconfig.symlink
+        sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GPGSIGN/$git_gpgsign/g" -e "s/GPGKEY/$git_gpgkey/g" $DOTFILES_ROOT/git/gitconfig.symlink.template > $DOTFILES_ROOT/git/gitconfig.symlink
 
     fi
     return ${E_SUCCESS}
