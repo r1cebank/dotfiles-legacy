8073af446c5116ab3cee566678263acee6df3acb
r1cebank <siyuangao@gmail.com>
2017-12-18 20:12:58 -0800

    adding code extension installer

---
+++ tasks/09_code_install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+function code_install_init() {
+    task_setup "code_install" "Code Install" "Install code packages" "install_tools"
+}
+
+function code_install_run() {
+    # Check for code
+    if hash code >/dev/null 2>&1; then
+        log_info "Installing code extensions."
+        while read in; do code --install-extension "$in"; done < $DOTFILES_ROOT/code/extensions.list
+    else
+        log_info "Did not find code, skipping Code packages."
+    fi
+    return ${E_SUCCESS}
+}
