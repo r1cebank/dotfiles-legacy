e57129b9b68de5b3c1604a3790f70ddfbb042bd6
r1cebank <siyuangao@gmail.com>
2018-02-20 00:34:26 -0800

    move nodejs to nvm

--- nodejs/aliases.zsh
+++ nodejs/aliases.zsh
@@ -6,8 +6,3 @@ alias npm-i='npm install --save'
 alias npm-d='npm install --save-dev'
 alias npm-g='npm install --global'
 alias nrd='npm run dev'
-
-# ---------------------------------------------------------------------------- #
-# Add heroku to the PATH.                                                      #
-# ---------------------------------------------------------------------------- #
-export PATH="/usr/local/heroku/bin:$PATH"
--- tasks/07_install_nodejs.sh
+++ tasks/07_install_nodejs.sh
@@ -7,26 +7,24 @@ function install_nodejs_init() {
 function install_nodejs_run() {
     PLATFORM=$(settings_get "PLATFORM")
     # Check for n
-    if ! type "n" > /dev/null; then
-        log_info "Installing Node.js via n for you."
+    if ! type "nvm" > /dev/null; then
+        log_info "Installing Node.js via nvm for you."
 
         # Install n and latest LTS node.js
-        curl -L http://git.io/n-install | bash -s -- -n -y lts
+        curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 
         # Reload nodejs environment variables
-        source $HOME/.dotfiles/nodejs/env.zsh
+        export NVM_DIR="$HOME/.nvm"
+        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+        [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 
         # Install the last node lts
-        n lts
+        nvm install 8
+        nvm alias default 8
 
-        # Hide the folder so that it's not visible
-        if [ "$PLATFORM" = "darwin" ]; then
-            chflags hidden ~/n
-        else
-            echo "n" >> $HOME/.hidden
-        fi
     else
-        log_info "You already have n installed"
+        log_info "You already have nvm installed"
     fi
     return ${E_SUCCESS}
 }
