2efaeeb671aabc4023dbd05b4a5c7a01d1398458
r1cebank <siyuangao@gmail.com>
2018-03-01 23:03:26 -0800

    add flutter and dart

---
+++ dart/env.zsh
@@ -0,0 +1,1 @@
+export PATH=/usr/lib/dart/bin:$HOME/flutter/bin:$PATH
--- tasks/05_install_tools.sh
+++ tasks/05_install_tools.sh
@@ -56,6 +56,15 @@ function install_tools_run() {
         sudo dpkg -i hyper.deb
         rm hyper.deb
 
+        # Instal flutter
+        git clone -b beta https://github.com/flutter/flutter.git
+
+        # Adding dart
+        sudo sh -c 'curl https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
+        sudo sh -c 'curl https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
+        sudo apt-get update
+        sudo apt-get install -y dart
+
         # Adding chrome
         wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
         sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
