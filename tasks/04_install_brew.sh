b2ab1a4e652402856fdb38a77b8ca236d81e9705
Siyuan Gao <siyuan.gao@move.com>
2018-04-20 15:28:54 -0700

    Arch support

--- tasks/04_install_brew.sh
+++ tasks/04_install_brew.sh
@@ -8,10 +8,9 @@ function install_brew_run() {
     PLATFORM=$(settings_get "PLATFORM")
     # Check for Homebrew
     if ! hash brew >/dev/null 2>&1; then
-        log_info "Installing Homebrew for you."
-
         # Install the correct homebrew for each OS type
         if [ "$PLATFORM" = "darwin" ]; then
+            log_info "Installing Homebrew for you."
             ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
         else
             log_info "Linux systems does not require install of homebrew"
