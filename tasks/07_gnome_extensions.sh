5fb5e92bca39c6391326486ed20538711f6ee62b
r1cebank <siyuangao@gmail.com>
2020-06-27 16:35:49 -0700

    fix gnome extensions

--- tasks/07_gnome_extensions.sh
+++ tasks/07_gnome_extensions.sh
@@ -9,7 +9,7 @@ function gnome_extensions_run() {
     mkdir -p $HOME/.local/share/gnome-shell/extensions
     if hash gnome-shell >/dev/null 2>&1; then
         log_info "Installing gnome extensions."
-        while read in; do $DOTFILES_ROOT/bin/gnome-ext-install install "$in"; done < $DOTFILES_ROOT/gnome/extensions.list
+        while read in; do $DOTFILES_ROOT/bin/gnomex install "$in"; done < $DOTFILES_ROOT/gnome/extensions.list
         # gnome-shell --replace &>/dev/null & disown
     else
         log_info "Did not find gnome, skipping gnome extensions."
