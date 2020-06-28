3f7d00a565f4be93646e025f5e8527567fe9c718
r1cebank <siyuangao@gmail.com>
2020-06-28 11:53:16 -0700

    Fix various issues

---
+++ tasks/12_wrap_up.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+function wrap_up_init() {
+    task_setup "wrap_up" "Final wrap up" "Finishing install" "check_system"
+}
+
+function wrap_up_run() {
+    PLATFORM=$(settings_get "PLATFORM")
+    if [ "$PLATFORM" = "debian" ]; then
+        if ask "Reboot?"; then
+            sudo reboot
+        fi
+    elif [ "$PLATFORM" = "arch" ]; then
+        # Fix yubikey-agent service
+        sudo sed -i 's/multi-user.target/default.target/g' /usr/lib/systemd/user/yubikey-agent.service
+        systemctl daemon-reload --user
+        systemctl --user enable --now yubikey-agent
+        if ask "Reboot?"; then
+            sudo reboot
+        fi
+    fi
+    return ${E_SUCCESS}
+}
