f00f3b39b3f3861e9104af486f60c98700de92b8
r1cebank <siyuangao@gmail.com>
2020-09-30 20:48:44 -0700

    using zip ext since expand-archive wont work with tmp

--- setup.ps1
+++ setup.ps1
@@ -1,8 +1,8 @@
 $downloadDest = [System.IO.Path]::GetTempFileName()
 $extractDest = [System.IO.Path]::GetTempPath()
 ### Download the archive
-(New-Object Net.WebClient).DownloadFile("https://github.com/r1cebank/dotfiles/archive/master.zip", "$downloadDest")
-Expand-Archive -LiteralPath $downloadDest -DestinationPath $extractDest
+(New-Object Net.WebClient).DownloadFile("https://github.com/r1cebank/dotfiles/archive/master.zip", "$downloadDest.zip")
+Expand-Archive -LiteralPath "$downloadDest.zip" -DestinationPath $extractDest
 
 ### Run the installation
 Invoke-Expression -Command $extractDest\dotfiles-master\installer.ps1
