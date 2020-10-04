529525d63eb07d5cd330b82e7dd973915f9efb82
r1cebank <siyuangao@gmail.com>
2020-10-03 21:44:48 -0700

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -1,7 +1,7 @@
 param($Step="A")
 
 ### Self elevating script
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -NoExit -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs -ArgumentList $Step; exit }
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -NoExit -ExecutionPolicy Bypass -File `"$PSCommandPath -Step $Step`"" -Verb RunAs; exit }
 # -------------------------------------
 # Imports
 # -------------------------------------
