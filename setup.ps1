598557faee7e908a63005abd2ce53e7748f1b405
r1cebank <siyuangao@gmail.com>
2020-10-03 17:54:49 -0700

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -4,5 +4,6 @@ $extractDest = [System.IO.Path]::GetTempPath() + [System.Guid]::NewGuid().ToStri
 (New-Object Net.WebClient).DownloadFile("https://github.com/r1cebank/dotfiles/archive/master.zip", "$downloadDest.zip")
 Expand-Archive -LiteralPath "$downloadDest.zip" -DestinationPath $extractDest
 
+Set-Location -Path $extractDest\dotfiles-master
 ### Run the installation
 Invoke-Expression -Command $extractDest\dotfiles-master\installer.ps1
