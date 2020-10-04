b91af12a1a7d484f157c2688ab89121f0184dc04
r1cebank <siyuangao@gmail.com>
2020-10-03 18:50:19 -0700

    multi stage install

--- installer.ps1
+++ installer.ps1
@@ -1,3 +1,72 @@
+# -------------------------------------
+# Function Globals
+# -------------------------------------
+$global:started = $FALSE
+$global:startingStep = $Step
+$global:restartKey = "Restart-And-Resume"
+$global:RegRunKey ="HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
+$global:powershell = (Join-Path $env:windir "system32\WindowsPowerShell\v1.0\powershell.exe")
+
+
+# -------------------------------------
+# Collection of Utility functions.
+# -------------------------------------
+function Should-Run-Step([string] $prospectStep) 
+{
+	if ($global:startingStep -eq $prospectStep -or $global:started) {
+		$global:started = $TRUE
+	}
+	return $global:started
+}
+
+function Wait-For-Keypress([string] $message, [bool] $shouldExit=$FALSE) 
+{
+	Write-Host "$message" -foregroundcolor yellow
+	$key = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+	if ($shouldExit) {
+		exit
+	}
+}
+
+function Test-Key([string] $path, [string] $key)
+{
+    return ((Test-Path $path) -and ((Get-Key $path $key) -ne $null))   
+}
+
+function Remove-Key([string] $path, [string] $key)
+{
+	Remove-ItemProperty -path $path -name $key
+}
+
+function Set-Key([string] $path, [string] $key, [string] $value) 
+{
+	Set-ItemProperty -path $path -name $key -value $value
+}
+
+function Get-Key([string] $path, [string] $key) 
+{
+	return (Get-ItemProperty $path).$key
+}
+
+function Restart-And-Run([string] $key, [string] $run) 
+{
+	Set-Key $global:RegRunKey $key $run
+	Restart-Computer
+	exit
+} 
+
+function Clear-Any-Restart([string] $key=$global:restartKey) 
+{
+	if (Test-Key $global:RegRunKey $key) {
+		Remove-Key $global:RegRunKey $key
+	}
+}
+
+function Restart-And-Resume([string] $script, [string] $step) 
+{
+	Restart-And-Run $global:restartKey "$global:powershell $script -Step $step"
+}
+
 function Verify-Elevated {
     # Get the ID and security principal of the current user account
     $myIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
@@ -33,26 +102,44 @@ if (!(Verify-Elevated)) {
     exit
 }
 
-### Chocolatey
-Write-Host "Installing Chocolatey..." -ForegroundColor "Yellow"
-if ((which cinst) -eq $null) {
-    iex (new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')
-    Refresh-Environment
-    choco feature enable -n=allowGlobalConfirmation
+param($Step="A")
+Clear-Any-Restart
+
+if (Should-Run-Step "A") 
+{
+    ### Chocolatey
+    Write-Host "Installing Chocolatey..." -ForegroundColor "Yellow"
+    if ((which cinst) -eq $null) {
+        iex (new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')
+        Refresh-Environment
+        choco feature enable -n=allowGlobalConfirmation
+    }
+
+    ### Install Pinned Applications
+    foreach ($line in Get-Content .\windows\packages\packages.pinned) {
+        choco install $line --limit-output; <# pin; evergreen #> choco pin add --name $line --limit-output
+    }
+
+    ### Install Regular Applications
+    foreach ($line in Get-Content .\windows\packages\packages.list) {
+        choco install $line --limit-output
+    }
+
+    ### Show desktop icons
+    Invoke-Expression -Command .\windows\settings\ShowIcon.ps1
+
+    ### Disable UAC
+    Invoke-Expression -Command .\windows\settings\DisableUAC.ps1
+	Wait-For-Keypress "The test script will continue after a reboot, press any key to reboot..." 
+	Restart-And-Resume $script "B"
 }
 
-### Install Pinned Applications
-foreach ($line in Get-Content .\windows\packages\packages.pinned) {
-    choco install $line --limit-output; <# pin; evergreen #> choco pin add --name $line --limit-output
+if (Should-Run-Step "B") 
+{
+	### Install Post Reboot Applications
+    foreach ($line in Get-Content .\windows\packages\packages.postreboot) {
+        choco install $line --limit-output
+    }
 }
 
-### Install Regular Applications
-foreach ($line in Get-Content .\windows\packages\packages.list) {
-    choco install $line --limit-output
-}
-
-### Show desktop icons
-Invoke-Expression -Command .\windows\settings\ShowIcon.ps1
-
-### Disable UAC
-Invoke-Expression -Command .\windows\settings\DisableUAC.ps1
+Wait-For-Keypress "Install script Complete, press any key to exit script..."
