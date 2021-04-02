21fecb8e0485c4bb81c520cb176a0de208fe409d
Siyuan Gao <siyuangao@protonmail.com>
2021-04-01 22:28:05 -0700

    Update path.zsh

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,2 +1,3 @@
 # Default PATH
 export PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/lib/jvm/java-8-openjdk/bin:$HOME/Android/Sdk/tools:$HOME/Android/Sdk/platform-tools:$HOME/bin:$HOME/.nimble/bin:$HOME/.cargo/bin"
+export PATH=$PATH:$(go env GOPATH)/bin
